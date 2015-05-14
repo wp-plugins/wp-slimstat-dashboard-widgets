516bbeab8473a90e99709adf0fa8e5c7d584a885
coolmann <coolmann@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-14 01:23:01 +0000

    Version 3.2 - Making it clear that the plugin has been discontinued.
    git-svn-id: https://plugins.svn.wordpress.org/wp-slimstat-dashboard-widgets/trunk@1160014 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WP SlimStat Dashboard Widgets
 Plugin URI: http://wordpress.org/plugins/wp-slimstat-dashboard-widgets/
-Description: Monitor your visitors from your Wordpress dashboard. Requires WP SlimStat 3.5.2+
+Description: This plugin has been discontinued. Dashboard widgets are now available directly in Slimstat 4.0
 Version: 3.2
 Author: Camu
 Author URI: http://slimstat.getused.to.it
