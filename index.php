3f788dcc5c6a3c59ba189832c6801a8d89267eff
coolmann <coolmann@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-14 03:20:05 +0000

    Version 3.2 - Fixed a typo
    git-svn-id: https://plugins.svn.wordpress.org/wp-slimstat-dashboard-widgets/trunk@1160074 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -9,6 +9,6 @@ Author URI: http://slimstat.getused.to.it
 */
 
 function wp_slimstat_dashboard_widgets_discontinued(){
-	echo '<div class="updated slimstat-notice" style="padding:10px"><span>WP Slimstat Dashboard Widgets add-on has been discontinued. Its functionality is now available directly in Slimstat 4.0. Please uninstall the add-on to make this message disappear.</span></div>';
+	echo '<div class="updated slimstat-notice" style="padding:10px"><span>WP Slimstat Dashboard Widgets has been discontinued. Its functionality is now available directly in Slimstat 4.0. Please uninstall the add-on to make this message disappear.</span></div>';
 }
 add_action('admin_notices', 'wp_slimstat_dashboard_widgets_discontinued', 10);
