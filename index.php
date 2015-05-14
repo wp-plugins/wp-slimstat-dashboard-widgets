009851711f8c4481ac3975063f37c2669c152a97
coolmann <coolmann@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-14 03:18:04 +0000

    Version 3.2 - Added a nag message to convince users to uninstall the add-on.
    git-svn-id: https://plugins.svn.wordpress.org/wp-slimstat-dashboard-widgets/trunk@1160070 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -8,4 +8,7 @@ Author: Camu
 Author URI: http://slimstat.getused.to.it
 */
 
-// This plugin has been discontinued. Its functionality has been rolled into Slimstat 4.0.
+function wp_slimstat_dashboard_widgets_discontinued(){
+	echo '<div class="updated slimstat-notice" style="padding:10px"><span>WP Slimstat Dashboard Widgets add-on has been discontinued. Its functionality is now available directly in Slimstat 4.0. Please uninstall the add-on to make this message disappear.</span></div>';
+}
+add_action('admin_notices', 'wp_slimstat_dashboard_widgets_discontinued', 10);
